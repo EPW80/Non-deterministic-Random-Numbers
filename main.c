717d22312153d7e51794d457ef8d366ca986c5e4
EPW80 <erikpw009@gmail.com>
2024-04-06 00:14:17 -0700

    additional info

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
+// Program name: "Random Number Normalization". This program fill an array with up to
+// element with a random number and normalize them into a number within the range of
+// 1.0 and 2.0
+// Copyright (C) <2023>  <Erik Williams>
+
+// This file is part of the software program "Random Number Normalization".
+
+// Random Number Normalization is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Random Number Normalization is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 //   Author information
 //   Author name : Erik Williams
 //   Author email: epwilliams@csu.fullerton.edu
@@ -6,7 +26,8 @@
 #include "stdio.h"
 extern char *executive();
 
-int main() {
+int main()
+{
     printf("Welcome to Random Products, LLC.\n");
     printf("This software is maintained by Erik Williams\n\n");
 
